2f92e49ebcf79c8469b5c6e98bd386663baab6f0
kekit <70204612+k3kit@users.noreply.github.com>
2023-06-29 18:05:14 +0400

    feat: add create server

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const products = require("./data/products");
+const server = http.createServer((req, res) => {
+  if (req.url === "/api/products"  && req.method === 'GET') {
+    res.writeHead(200, { "Content-Type": "applitcation/json" });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "Content-Type": "applitcation/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
